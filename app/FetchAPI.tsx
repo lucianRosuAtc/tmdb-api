15ea74f889eb94234a407675247de47d6bac2fe6
Lucian Rosu <lucian.rosu@adopstar.com>
2024-02-16 10:56:39 +0000

    Refactor API fetching and display movie details

--- app/FetchAPI.tsx
+++ app/FetchAPI.tsx
@@ -1,32 +1,32 @@
 
-import { Interface } from 'readline';
-import { json } from 'stream/consumers';
-const fetch = require('node-fetch');
+// import { Interface } from 'readline';
+// import { json } from 'stream/consumers';
+// const fetch = require('node-fetch');
 
-interface Movie {
-  poster_path: string;
-  // Add other movie properties here as needed
-}
+// interface Movie {
+//   poster_path: string;
+//   // Add other movie properties here as needed
+// }
 
-export default async function getData() {
-const movieId = 5;
-      const url = `https://api.themoviedb.org/3/account/${process.env.API_ACCESS_TOKEN}`;
-      const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYmY1YTUyODA2MjliZTI0MzgzZTNiODg0MjRkNzZkYyIsInN1YiI6IjY1Y2RmMTYwMzEyMzQ1MDE3YmJiMTQ3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0D7bGL73GTzarWR45vzU9Sj2Rf_OAvCXnuabutkrIAY'
-        }
-      };
+// export default async function getData() {
+// const movieId = 5;
+//       const url = `https://api.themoviedb.org/3/account/${process.env.API_ACCESS_TOKEN}`;
+//       const options = {
+//         method: 'GET',
+//         headers: {
+//           accept: 'application/json',
+//           Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYmY1YTUyODA2MjliZTI0MzgzZTNiODg0MjRkNzZkYyIsInN1YiI6IjY1Y2RmMTYwMzEyMzQ1MDE3YmJiMTQ3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0D7bGL73GTzarWR45vzU9Sj2Rf_OAvCXnuabutkrIAY'
+//         }
+//       };
       
-    // Then you can use this interface with your fetch function
-    fetch(url, options)
-    .then((res: Response) => res.json())
-    .then((json: Movie) => {
-      const posterUrl = `https://image.tmdb.org/t/p/w500${json.poster_path}`;
-      console.log(posterUrl);
-    })
-    .catch((err: Error) => console.error('error:' + err));
-}
+//     // Then you can use this interface with your fetch function
+//     fetch(url, options)
+//     .then((res: Response) => res.json())
+//     .then((json: Movie) => {
+//       const posterUrl = `https://image.tmdb.org/t/p/w500${json.poster_path}`;
+//       console.log(posterUrl);
+//     })
+//     .catch((err: Error) => console.error('error:' + err));
+// }
 
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,55 @@
 import Image from "next/image";
-import { FetchAPI } from "./FetchAPI";
 
-export default function Home() {
+type Movie = {
+  id: number;
+  original_title: string;
+  genre_ids: number[];
+  poster_path: string;
+  vote_average: number;
+  vote_count: number;
+};
+
+const getMovie = async (): Promise<Movie[]> => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Error fetching data");
+  }
+  const data = await res.json();
+  return data.results;
+};
+
+export default async function Home() {
+  const movies = await getMovie();
+
   return (
-    <main>
-      <FetchAPI /> 
+    <main className="">
+      <h1 className="flex justify-center my-10 text-xl md:text-5xl text-white">TMDB API</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 m-20">
+
+      {(await getMovie()).map((movie) => (
+        <div key={movie.id} className="text-white">
+        
+          <div className="aspect-video relative">
+            <Image
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.original_title}
+              fill
+              className="object-cover rounded-md border border-orange-500"
+              />
+          </div>
+          <div className="flex flex-col justify-center mt-3 mb-10">
+
+          <h2>Title: {movie.original_title}</h2>
+          <h3>Genre: {movie.genre_ids}</h3>
+          <h3>Average vote: {movie.vote_average}</h3>
+          <h3>Vote count: {movie.vote_count}</h3>
+          </div>
+        </div>
+      ))}
+      </div>
     </main>
   );
 }
