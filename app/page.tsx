c62d8fa5a4729ad0d31a7043497187d3c11576f6
Lucian Rosu <lucian.rosu@adopstar.com>
2024-02-16 12:50:47 +0000

    Update grid layout and image styling in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default async function Home() {
       <h1 className="flex justify-center my-10 text-xl md:text-5xl text-white">
         TMDB API
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 m-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 m-20 ">
         {(await getMovie()).map((movie) => (
           <div key={movie.id} className="text-white">
             <div className="aspect-square relative">
@@ -41,7 +41,7 @@ export default async function Home() {
                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                 alt={movie.original_title}
                 fill
-                className="object-cover rounded-md border border-orange-500"
+                className="object-contain rounded-md border border-orange-500 "
               />
             </div>
             <div className="flex flex-col justify-center mt-3 mb-10">
