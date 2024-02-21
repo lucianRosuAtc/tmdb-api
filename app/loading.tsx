219926af0c66fa143205e53a61460210d8882ea8
Lucian Rosu <lucian.rosu@adopstar.com>
2024-02-21 12:55:05 +0000

    Refactor Loading component layout

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,17 +2,17 @@ import { Skeleton } from"@/components/ui/skeleton"
 
 export default function Loading() {
   return (
-    <div className="">
-    <h1 className="flex justify-center my-10 text-xl md:text-5xl text-white"></h1>
-    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 m-20">
+<div className="">
+<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 px-4">
 
     {Array.from({length: 12}, (_, i) => i + 1).map((id) => (
-      <div key={id} className="text-white">
+      <div key={id}>
       
-        <div className="aspect-square relative rounded-md bg-gray-600 border border-orange-500">
-            <Skeleton className="object-cover" />
+        <div className="h-[450px] w-[300px] relative rounded-md bg-gray-600 border border-orange-500 mx-auto">
+          
+            <Skeleton className="object-cover"/>
         </div>
-        <div className="flex flex-col justify-center mt-3 mb-10">
+        <div className="flex flex-col justify-center items-center md:items-start mt-3 mb-10">
               <Skeleton className="h-6 my-2 w-1/2 bg-gray-500 rounded-md border border-orange-500"></Skeleton>
               <Skeleton className="h-6 my-2 w-1/2 bg-gray-500 rounded-md border border-orange-500"></Skeleton>
               <Skeleton className="h-6 my-2 w-1/2 bg-gray-500 rounded-md border border-orange-500"></Skeleton>
@@ -26,3 +26,6 @@ export default function Loading() {
   </div>
   )
 }
+
+
+
