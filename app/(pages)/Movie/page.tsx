abaeab25b73d8e42dfcd5c30e798ec0492e6aa1c
Lucian Rosu <lucian.rosu@adopstar.com>
2024-02-22 11:03:35 +0000

    Add hero image and update heading in Movie page

--- app/(pages)/Movie/page.tsx
+++ app/(pages)/Movie/page.tsx
@@ -1,13 +1,21 @@
 import FetchAPI from "@/components/FetchAPI";
 import Loading from "@/app/loading";
 import { Suspense } from "react";
+import Image from "next/image";
 
 export default async function page() {
   return (
     <main>
-      <h1 className="flex justify-center my-10 text-xl md:text-5xl text-white">
-        New Movies
-      </h1>
+
+<div className="flex items-end justify-center relative pb-10">
+      
+        <Image src="/img/hero/marvelsLogo.png" alt="hero" width={1920} height={680} className=""/>
+  <h1 className="flex justify-center my-10 text-xl md:text-8xl text-white absolute">
+      New Movies
+      </h1> 
+      </div>
+
+  
       <Suspense fallback={<Loading />}>
         <FetchAPI
           apiUrl={`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}`}
